cecf5012f8a5e194683ecb6aed37fefc8bc2aa1a
Stijn De Clercq <stijn.declercq@ugent.be>
2020-07-25 16:48:33 +0200

    config.py

--- config.py
+++ config.py
@@ -3,6 +3,11 @@ import sys
 
 if len(sys.argv[1]) != 2:
     print("Invalid arguments.\nSyntax: python3 config.py path_to_your_file")
+    exit(1)
 
 with open("{}/filepath.txt", "w+") as file:
     file.write(sys.argv[1])
+
+print("Configured {} as the path to your Api Key. You can run this command at any time to change this in the future."
+      .format(sys.argv[1]))
+exit(0)
