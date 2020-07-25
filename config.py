cbd08394dee9fdfb3c89762f57971d95d6cae63b
Stijn De Clercq <stijn.declercq@ugent.be>
2020-07-25 16:51:33 +0200

    fixed typo

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import sys
 
 
-if len(sys.argv[1]) != 2:
+if len(sys.argv) != 2:
     print("Invalid arguments.\nSyntax: python3 config.py path_to_your_file")
     exit(1)
 
-with open("{}/filepath.txt", "w+") as file:
+with open("{}/filepath.txt".format(sys.path[0]), "w+") as file:
     file.write(sys.argv[1])
 
-print("Configured {} as the path to your Api Key. You can run this command at any time to change this in the future."
+print("Configured {} as the path to your Api Key.\nYou can run this command at any time to change this in the future."
       .format(sys.argv[1]))
 exit(0)
